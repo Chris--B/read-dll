58e9d4e5e540f4f6baaf9e18b515cbc3d3a9b6e7
Chris Butler <chrisbutler296@gmail.com>
2019-04-28 20:08:22 -0400

    ⚡️ Added ExtraData + Cleanup

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,13 @@
 )]
 
 use std::{
-    cmp,
     env,
     fs,
-    io::Read,
+    io::{
+        self,
+        Read,
+        Write,
+    },
 };
 
 use failure;
@@ -27,23 +30,40 @@ use goblin::Object;
 
 type Res<T> = Result<T, failure::Error>;
 
-#[derive(Clone, Debug)]
-enum FormatData {
+/// The format that was found
+#[derive(Copy, Clone, Debug)]
+enum BinaryKind {
     Pe,
     Elf,
     MachO,
     // MacFat // ??
 }
 
-#[derive(Clone, Debug, Default)]
-struct Symbol {
-    name:       String,
-    per_format: Option<FormatData>,
+/// Extra data that's unique to a format
+#[derive(Clone, Debug)]
+enum BinaryKindExtra {
+    Pe {
+        offset: usize,
+        rva:    usize,
+        size:   usize,
+    },
+    Elf {
+        st_info: u8,
+    },
+    MachO {},
+    // Others...?
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
+struct Symbol {
+    name:  String,
+    extra: BinaryKindExtra,
+}
+
+#[derive(Clone, Debug)]
 struct SharedLibrary {
     name:          Option<String>,
+    kind:          BinaryKind,
     symbols:       Vec<Symbol>,
     is_lib:        bool,
     is_64:         bool,
@@ -84,7 +104,19 @@ fn main() -> Res<()> {
 
     // if format == table
     {
-        display_as_table(&lib);
+        match display_as_table(&lib) {
+            // No issues print table
+            Ok(()) => {},
+            // We lost our connection to stdout.
+            // The most common cause of this - someone piped us into
+            // `head` or `less` and quit before we wrote everything.
+            // This is fine, and we should not complain about it.
+            Err(ref err) if err.kind() == io::ErrorKind::BrokenPipe => {},
+            // Something else failed - UNACCEPTABLE. COMPLAIN LOUDLY.
+            Err(err) => {
+                Err(err)?;
+            },
+        }
     }
 
     Ok(())
@@ -99,18 +131,19 @@ fn handle_pe(pe: &goblin::pe::PE) -> Res<SharedLibrary> {
                 // PE libraries can export functions only by ordinal, so they
                 // don't have a name. Don't care, just make it
                 // empty instead.
-                name:       export.name.unwrap_or_default().to_string(),
-                per_format: Some(FormatData::Pe),
-                /* per_format: FormatData::Pe {
-                 *     ordinal: export.ordinal,
-                 *     offset: export.offset,
-                 * } */
+                name:  export.name.unwrap_or_default().to_string(),
+                extra: BinaryKindExtra::Pe {
+                    offset: export.offset,
+                    rva:    export.rva,
+                    size:   export.size,
+                },
             }
         })
         .collect();
 
     Ok(SharedLibrary {
         name: pe.name.map(str::to_string),
+        kind: BinaryKind::Pe,
         symbols,
         is_lib: pe.is_lib,
         is_64: pe.is_64,
@@ -128,17 +161,17 @@ fn handle_elf(elf: &goblin::elf::Elf) -> Res<SharedLibrary> {
         .iter()
         .map(|dynsym| {
             Symbol {
-                name:       strings[dynsym.st_name].to_string(),
-                per_format: Some(FormatData::Elf),
-                /* per_format: Some(Format::Elf {
-                 *      st_info: dynsym.st_info,
-                 * }, */
+                name:  strings[dynsym.st_name].to_string(),
+                extra: BinaryKindExtra::Elf {
+                    st_info: dynsym.st_info,
+                },
             }
         })
         .collect();
 
     Ok(SharedLibrary {
         name: elf.soname.map(str::to_string),
+        kind: BinaryKind::Elf,
         symbols,
         is_lib: elf.is_lib,
         is_64: elf.is_64,
@@ -158,14 +191,15 @@ fn handle_macho(macho: &goblin::mach::MachO) -> Res<SharedLibrary> {
             //     // ...
             // };
             Symbol {
-                name:       export.name.clone(),
-                per_format: Some(FormatData::MachO),
+                name:  export.name.clone(),
+                extra: BinaryKindExtra::MachO {},
             }
         })
         .collect();
 
     Ok(SharedLibrary {
         name: macho.name.map(str::to_string),
+        kind: BinaryKind::MachO,
         symbols,
         is_lib: true, // ???
         is_64: macho.is_64,
@@ -173,80 +207,140 @@ fn handle_macho(macho: &goblin::mach::MachO) -> Res<SharedLibrary> {
     })
 }
 
-fn display_as_table(lib: &SharedLibrary) {
+/// Formats a table of exported symbols.
+///
+/// ### Example
+/// The following is a trimmed example from SDL on Windows
+/// ```
+///     + ------------------------------- +
+///     | SDL2.dll (x64 Little Endian)    |
+///     + ------------------- + --------- +
+///     |        Name         | Demangled |
+///     + ------------------- + --------- +
+///     | SDL_AddEventWatch   | *         |
+///     | SDL_AddHintCallback | *         |
+///     | SDL_AddTimer        | *         |
+///     | SDL_AllocFormat     | *         |
+///     | SDL_AllocPalette    | *         |
+///     | SDL_AllocRW         | *         |
+///     | SDL_AtomicAdd       | *         |
+///     | SDL_AtomicCAS       | *         |
+///     | SDL_AtomicCASPtr    | *         |
+///     | SDL_AtomicGet       | *         |
+///     | SDL_AtomicGetPtr    | *         |
+///     | SDL_AtomicLock      | *         |
+///     | SDL_AtomicSet       | *         |
+///     | SDL_AtomicSetPtr    | *         |
+///     | SDL_AtomicTryLock   | *         |
+///     + ------------------- + --------- +
+///     | Found 15 exported symbols       |
+///     + ------------------------------- +
+/// ```
+fn display_as_table(lib: &SharedLibrary) -> io::Result<()> {
+    let handle = io::stdout();
+    let mut stdout = handle.lock();
+
+    // ==== FORMATTING =========================================================
+    // Store width of each hard-coded column
     #[derive(Copy, Clone, Debug)]
     struct ColWidth {
         name:      usize,
         demangled: usize,
     }
+
+    const COLUMN_SYM_NAME: &str = "Name";
+    const COLUMN_DEMANGLED: &str = "Demangled";
+
     let mut colw = ColWidth {
-        name:      "Exported Name".len() + 2,
-        demangled: "Demangled".len() + 2,
+        name:      COLUMN_SYM_NAME.len(),
+        demangled: COLUMN_DEMANGLED.len(),
     };
+
+    // Size the columns from the data
     for symbol in &lib.symbols {
         colw.name = colw.name.max(symbol.name.len());
     }
-    colw.name = colw.name.min(60);
-    println!(
-        "Table format: {} entries: {}, {}",
-        lib.symbols.len(),
-        colw.name,
-        colw.demangled,
-    );
+    // If the symbol names are crazy, or we hit a bug, limit the name length.
+    colw.name = colw.name.min(100);
 
     // Each new column adds 3 to the width.
     // This does not include side padding.
     let table_width = colw.name + (3 + colw.demangled);
 
-    println!("+ {x:-<width$} +", x = "", width = table_width,);
+    // Title - goes in a separate cell at the top of the table.
     let title = format!(
-        "Symbols for: {} ({} {} Endian)",
+        "{} ({} {} Endian)",
         lib.name.as_ref().map(String::as_str).unwrap_or("<unnamed>"),
         if lib.is_64 { "x64" } else { "x86" },
         if lib.little_endian { "Little" } else { "Big" },
     );
-    println!("| {:<width$} | ", title, width = table_width);
 
-    let row_separator = format!(
+    // Footer - goes in a separate cell at the bottom of the table.
+    let footer = format!("Found {} exported symbols", lib.symbols.len());
+
+    // Separates groups of rows/cells with mid junctions
+    let section_separator = format!(
         "+ {x:-<wname$} + {x:-<wdemangled$} +",
         x = "",
         wname = colw.name,
         wdemangled = colw.demangled,
     );
 
-    // Top of the header
-    println!(
-        "+ {x:-<wname$} + {x:-<wdemangled$} +",
-        x = "",
-        wname = colw.name,
-        wdemangled = colw.demangled,
-    );
-    // Header with column labels
-    println!(
-        "| {:^wname$} | {:^wdemangled$} |",
-        "Symbol Name",
-        "Demangled",
-        wname = colw.name,
-        wdemangled = colw.demangled,
-    );
+    // ==== TABLE HEADER =======================================================
+    {
+        // Top line, no mid-junctions
+        writeln!(stdout, "+ {x:-<width$} +", x = "", width = table_width,)?;
+        // Title
+        writeln!(stdout, "| {:<width$} | ", title, width = table_width)?;
 
-    // Bottom of header
-    println!("{}", row_separator);
-
-    // Each row
-    for symbol in &lib.symbols {
-        // TODO: Output this
-        let _per_format = &symbol.per_format;
-        println!(
-            "| {:<wname$} | {:<wdemangled$} |",
-            symbol.name,
-            // TODO: demangle(symbol.name),
-            "*",
+        // Title and labels separator
+        writeln!(
+            stdout,
+            "+ {x:-<wname$} + {x:-<wdemangled$} +",
+            x = "",
             wname = colw.name,
             wdemangled = colw.demangled,
-        );
+        )?;
+        // Labels
+        writeln!(
+            stdout,
+            "| {:^wname$} | {:^wdemangled$} |",
+            COLUMN_SYM_NAME,
+            COLUMN_DEMANGLED,
+            wname = colw.name,
+            wdemangled = colw.demangled,
+        )?;
+
+        // Bottom line of header
+        writeln!(stdout, "{}", section_separator)?;
     }
 
-    // Bottom of the table
-    println!("{}", row_separator);
+    // ==== TABLE ROWS =========================================================
+    {
+        for symbol in &lib.symbols {
+            writeln!(
+                stdout,
+                "| {:<wname$} | {:<wdemangled$} |",
+                symbol.name,
+                // TODO: demangle(symbol.name),
+                "*",
+                wname = colw.name,
+                wdemangled = colw.demangled,
+            )?;
+        }
+    }
+
+    // ==== TABLE FOOTER ======================================================
+    {
+        // Bottom line, mid junctions
+        writeln!(stdout, "{}", section_separator)?;
+
+        // Footer
+        writeln!(stdout, "| {:<width$} | ", footer, width = table_width)?;
+        // Bottom line, no mid-junctions
+        writeln!(stdout, "+ {x:-<width$} +", x = "", width = table_width,)?;
+    }
+
+    // Table is done, drop the lock on stdout on return
+    Ok(())
 }
